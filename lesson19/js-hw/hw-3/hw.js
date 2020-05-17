cd7c73ce767d01d989d0c221883497765d36e99b
SKryzhantovskiy <stanislawww2007@mail.ru>
2020-05-17 22:06:32 +0300

    correction after comments

--- lesson19/js-hw/hw-3/hw.js
+++ lesson19/js-hw/hw-3/hw.js
@@ -7,17 +7,17 @@
 */
 
 //var age = 20;
-let age = 20;
+const age = 20;
 //var text = 'Ваш возраст: ' + Age + ' лет';
-let text = `Ваш возраст: ${age} лет`;
+const text = `Ваш возраст: ${age} лет`;
 alert(text); // Должен вывести: Ваш возраст: 20 лет
 
 //var hours = 12;
 let hours = 12;
 //var minutes = 45;
-var minutes = 45;
-hours = hours + 1;
+const minutes = 45;
+hours += 1;
 //var textwithtime = 'Текущее время: ' + hours + ':' + minutes;
-let textWithTime = `Текущее время: ${hours}:${minutes}`;
+const textWithTime = `Текущее время: ${hours}:${minutes}`;
 //alert(textwithtime); // Должен вывести: Текущее время: 13:45
 alert(textWithTime);
