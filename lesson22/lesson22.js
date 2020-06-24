4ca33ed952260ac7b2d531722f26faab3038ebbb
SKryzhantovskiy <61256848+SKryzhantovskiy@users.noreply.github.com>
2020-06-25 00:20:57 +0300

    lesson22

---
+++ lesson22/lesson22.js
@@ -0,0 +1,53 @@
+// Задание 1
+
+let i = 0;
+
+for (let i=10; i <= 50; i=i+10) {
+  console.log(i);
+}
+
+// Ожидаемый результат: 10 20 30 40 50
+
+// Задание 2
+/*
+ Написать функцию-конструктор Men с ДВУМЯ аргументами
+  С помощью которой можно создать объект
+  { profession: 'hero', name: 'Кларк', secondName: 'Кент' }
+*/
+function Men (name, secondName) {
+  this.profession = 'hero';
+  this.name = name;
+  this.secondName = secondName;  
+};
+const superman = new Men('Кларк', 'Кент');
+console.log('superman: ', superman);
+// Men { profession: 'hero', name: 'Кларк', secondName: 'Кент' }
+
+
+// Задание 3
+// Напишите функцию sum, которая работает так: sum(a)(b) возвращает a + b.
+
+function sum(a) {
+  let currentSum = a;
+  function f(b) {
+    currentSum += b;
+    return f;
+  }
+
+  f.toString = function() {
+    return currentSum;
+  };
+
+  return f;
+}
+console.log( sum(1)(2) );  // 3
+console.log( sum(5)(-1) ); // 4
+
+
+// Задание 4
+// Измените код так, чтобы console.log выводил последовательно символы 'П т и ц а'
+const bird = ['П', 'т', 'и', 'ц', 'а'];
+
+for (var index = 0; index < bird.length; index++) {
+    console.log(bird[index]);  
+}
